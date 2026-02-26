d42a12f282f08878791a3b34d7af9589f663abb9
Fouji5131 <mabdullah5131@gmail.com>
2026-02-26 22:45:14 +0500

    fastapi-task-manager-api

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
+class Task(BaseModel):
+    id: int
+    title: str
+    completed: bool
+
+tasks = [
+    # {"id": 1, "title": "Learn FastAPI", "completed": False},
+    # {"id": 2, "title": "Build AI API", "completed": False}
+]
+
+@app.post("/tasks")
+def create_task(task: Task):
+    tasks.append(task.model_dump())
+    return {"message": "Task created", "task": task}
+
+
 @app.get("/")
 def home():
-    return {
-        "message": "Hello, AI backend world!"
-    }
+    return {"message": "Task Manager API"}
 
-@app.get("/about")
-def about():
-    return {
-        "name": "Abdullah",
-        "goal": "AI Engineer"
-    }
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
 
-@app.get("/square/{number}")
-def square(number: int):
-    return {
-        "number": number,
-        "square": number * number
-    }
+
+@app.get("/tasks/{task_id}")
+def get_task(task_id: int):
+    for task in tasks:
+        if task["id"] == task_id:
+            return task
+    return {"error": "Task not found"}
+
+@app.get("/tasks/filter")
+def filter_tasks(completed: bool):
+    filtered = [task for task in tasks if task["completed"] == completed]
+    return {"filtered_tasks": filtered}
+
+# @app.get("/")
+# def home():
+#     return {
+#         "message": "Hello, AI backend world!"
+#     }
+
+# @app.get("/about")
+# def about():
+#     return {
+#         "name": "Abdullah",
+#         "goal": "AI Engineer"
+#     }
+
+# @app.get("/square/{number}")
+# def square(number: int):
+#     return {
+#         "number": number,
+#         "square": number * number
+#     }
