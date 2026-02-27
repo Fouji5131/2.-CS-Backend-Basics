47024ff462fe1c641cea24b31bb6b78ce9fef5f3
Fouji5131 <mabdullah5131@gmail.com>
2026-02-27 11:00:38 +0500

    Completed CRUD operations in fast API

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
-class Task(BaseModel):
+class TaskCreate(BaseModel):
+    title: str = Field(..., min_length=3, max_length=100)
+    complete: bool = False
+
+class Task(TaskCreate):
     id: int
-    title: str
-    completed: bool
 
 tasks = [
     # {"id": 1, "title": "Learn FastAPI", "completed": False},
@@ -14,31 +17,53 @@ tasks = [
 ]
 
 @app.post("/tasks")
-def create_task(task: Task):
-    tasks.append(task.model_dump())
-    return {"message": "Task created", "task": task}
+def create_task(task: TaskCreate):
+    new_task = {
+        "id": len(tasks) + 1,
+        **task.model_dump()
+    }
+    tasks.append(new_task)
+    return {"message": "Task created", "task": new_task}
+
+
+@app.get("/tasks")
+def get_tasks():
+    return {"tasks": tasks}
+
+@app.put("/tasks/{task_id}")
+def update_task(task_id: int, updated_task: TaskCreate):
+    for task in tasks:
+        if task["id"] == task_id:
+            task["title"] = updated_task.title
+            task["completed"] = updated_task.completed
+            return task
+    return {"error": "Task not found"}
+
+@app.delete("/tasks/{task_id}")
+def delete_task(task_id: int):
+    for index, task in enumerate(tasks):
+        if task["id"] == task_id:
+            tasks.pop(index)
+            return {"message": "Task deleted"}
+    return {"error": "Task not found"}
+
 
 
 @app.get("/")
 def home():
     return {"message": "Task Manager API"}
 
-@app.get("/tasks")
-def get_tasks():
-    return {"tasks": tasks}
+# @app.get("/tasks/{task_id}")
+# def get_task(task_id: int):
+#     for task in tasks:
+#         if task["id"] == task_id:
+#             return task
+#     return {"error": "Task not found"}
 
-
-@app.get("/tasks/{task_id}")
-def get_task(task_id: int):
-    for task in tasks:
-        if task["id"] == task_id:
-            return task
-    return {"error": "Task not found"}
-
-@app.get("/tasks/filter")
-def filter_tasks(completed: bool):
-    filtered = [task for task in tasks if task["completed"] == completed]
-    return {"filtered_tasks": filtered}
+# @app.get("/tasks/filter")
+# def filter_tasks(completed: bool):
+#     filtered = [task for task in tasks if task["completed"] == completed]
+#     return {"filtered_tasks": filtered}
 
 # @app.get("/")
 # def home():
